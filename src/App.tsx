f124b301cbefc2bba9748f598ac29e6e7a97feeb
Natalia Taira <nataliagtaira@gmail.com>
2025-07-30 14:07:36 -0300

    Finaliza rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
-import Home from './pages/home/Home'
+
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+
+import Cadastro from './pages/cadastro/Cadastro';
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
 
 function App() {
   return (
-      <Home />
+    <>
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
   )
 }
 
